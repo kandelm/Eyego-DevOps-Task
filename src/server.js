804ce60ec26ada3fb16abc6a38dc893d3b8b6199
mahmoud kandel <119636781+kandelm@users.noreply.github.com>
2025-07-23 12:40:35 +0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello Eyego Test' });
+});
+
+app.get('/', (req, res) => {  
   res.json({ message: 'Hello Eyego' });
 });
 
